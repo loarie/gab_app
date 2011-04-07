e5838400eaf4455d11dbb577bd27555415c1a94f
loarie <loarie@gmail.com>
2011-04-06 17:03:18 -0700

    altered home route for a third time (having trouble deploying this on heroku)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'pages', :action => 'home'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.home   '/home',   :controller => 'pages', :action => 'home'
   # The priority is based upon order of creation: first created -> highest priority.
